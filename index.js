fd2a70320b044e33faa1ce8ffcf9d5990ea32223
Kyle Herock <kherock@gmail.com>
2018-09-15 22:34:52 -0400

    Add ESLint and Prettier

--- index.js
+++ index.js
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-
+'use strict';
 module.exports = require('./lib/appmetrics-elk.js');
